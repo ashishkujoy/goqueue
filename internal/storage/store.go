583bcc5b8b45582cfc95a7803f61c9244ca87545
ashish <ashishkujoy@gmail.com>
2025-05-02 15:56:48 +0530

    Implements store

---
+++ internal/storage/store.go
@@ -0,0 +1,89 @@
+package storage
+
+import (
+	"encoding/binary"
+	"os"
+)
+
+type Store struct {
+	reader *os.File
+	writer *os.File
+	offset int
+}
+
+func NewStore(filePath string) (*Store, error) {
+	writer, err := os.OpenFile(filePath, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		return nil, err
+	}
+
+	reader, err := os.OpenFile(filePath, os.O_RDONLY, 0644)
+	if err != nil {
+		return nil, err
+	}
+
+	stat, err := reader.Stat()
+	if err != nil {
+		return nil, err
+	}
+
+	return &Store{
+		reader: reader,
+		writer: writer,
+		offset: int(stat.Size()),
+	}, nil
+}
+
+func (s *Store) Append(data []byte) (int, error) {
+	currentOffset := s.offset
+	sizeBuff := make([]byte, 4)
+
+	binary.BigEndian.PutUint32(sizeBuff, uint32(len(data)))
+	n, err := s.writer.Write(sizeBuff)
+	if err != nil {
+		return 0, err
+	}
+	s.offset += n
+
+	n, err = s.writer.Write(data)
+	if err != nil {
+		return 0, err
+	}
+	s.offset += n
+
+	return currentOffset, nil
+}
+
+func (s *Store) Read(offset int) ([]byte, error) {
+	sizeBuff := make([]byte, 4)
+	n, err := s.reader.ReadAt(sizeBuff, int64(offset))
+	if err != nil {
+		return nil, err
+	}
+	offset += n
+
+	dataLen := binary.BigEndian.Uint32(sizeBuff)
+	data := make([]byte, dataLen)
+
+	_, err = s.reader.ReadAt(data, int64(offset))
+	if err != nil {
+		return nil, err
+	}
+
+	return data, err
+}
+
+func (s *Store) Flush() error {
+	return s.writer.Sync()
+}
+
+func (s *Store) Close() error {
+	if err := s.Flush(); err != nil {
+		return err
+	}
+	if err := s.reader.Close(); err != nil {
+		return err
+	}
+
+	return s.writer.Close()
+}
---
+++ internal/storage/store_test.go
@@ -0,0 +1,66 @@
+package storage
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAppendAndReadSingleEntry(t *testing.T) {
+	filePath := fmt.Sprintf("%s%s", os.TempDir(), "TestAppendAndReadSingleEntry")
+	defer os.Remove(filePath)
+	store, err := NewStore(filePath)
+
+	assert.NoError(t, err)
+
+	offset, err := store.Append([]byte("Hello World"))
+	assert.NoError(t, err)
+	assert.Equal(t, 0, offset)
+
+	data, err := store.Read(offset)
+	assert.NoError(t, err)
+	assert.Equal(t, data, []byte("Hello World"))
+}
+
+func TestAppendAndReadMultipleEntry(t *testing.T) {
+	filePath := fmt.Sprintf("%s%s", os.TempDir(), "TestAppendAndReadMultipleEntry")
+	defer os.Remove(filePath)
+	store, err := NewStore(filePath)
+	defer store.Close()
+
+	assert.NoError(t, err)
+
+	offset1, _ := store.Append([]byte("Hello World"))
+	offset2, _ := store.Append([]byte("Another Hello World"))
+	data1, err := store.Read(offset1)
+	assert.NoError(t, err)
+	data2, err := store.Read(offset2)
+	assert.NoError(t, err)
+	assert.Equal(t, data1, []byte("Hello World"))
+	assert.Equal(t, data2, []byte("Another Hello World"))
+}
+
+func TestReadFromANewStoreAfterClose(t *testing.T) {
+	filePath := fmt.Sprintf("%s%s", os.TempDir(), "TestReadFromANewStoreAfterClose")
+	defer os.Remove(filePath)
+	store, err := NewStore(filePath)
+	defer store.Close()
+
+	assert.NoError(t, err)
+
+	offset1, _ := store.Append([]byte("Hello World"))
+	offset2, _ := store.Append([]byte("Another Hello World"))
+	err = store.Close()
+	assert.NoError(t, err)
+
+	store, err = NewStore(filePath)
+
+	data1, err := store.Read(offset1)
+	assert.NoError(t, err)
+	data2, err := store.Read(offset2)
+	assert.NoError(t, err)
+	assert.Equal(t, data1, []byte("Hello World"))
+	assert.Equal(t, data2, []byte("Another Hello World"))
+}
